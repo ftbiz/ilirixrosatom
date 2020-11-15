5b5f534e2080d9f0dd4ff5014c14b86871dcbd61
Marina Ivanova <ivamarina22@gmail.com>
2020-11-15 03:25:02 +0300

    Add lines between parallel branches

--- Assets/Scripts/GraphVisualizer.cs
+++ Assets/Scripts/GraphVisualizer.cs
@@ -13,6 +13,8 @@ namespace GraphVisual
         public GameObject pointPrefab;
         public GameObject taskPrefab;
 
+        public GameObject LinePrefab;
+
         private Milestones milestones;
         private List<TaskItem> allTasks;
 
@@ -50,16 +52,39 @@ namespace GraphVisual
             }
 
             var startPoint = Instantiate(pointPrefab, defaultStartPosition, Quaternion.identity);
+            var needLine = false;
             
             foreach (var milestoneTask in milestoneTasks)
             {
-                if (!allTasks.Exists(x => x.id == milestoneTask.id))
+                if (!allTasks.Exists(x => x.ID == milestoneTask.id))
                 {
                     var taskObject = Instantiate(taskPrefab);
                     var taskItem = taskObject.GetComponent<TaskItem>();
-                    
+
                     var startPointPosition = startPoint.transform.position;
 
+                    if (milestoneTask.parentId != null)
+                    {
+                        var parentTask = allTasks.Find(x => x.ID == milestoneTask.parentId);
+
+                        if (parentTask != null)
+                        {
+                            if (parentTask.IsMultiParent)
+                            {
+                                if (parentTask.IsFirstChildSet)
+                                {
+                                    startPointPosition += new Vector3(0, distanceBetweenChains, 0);
+                                    startPoint = Instantiate(pointPrefab, startPointPosition, Quaternion.identity);
+                                    CreateLine(parentTask.EndPoint, startPointPosition);
+                                }
+                                else
+                                {
+                                    parentTask.IsFirstChildSet = true;
+                                }
+                            }
+                        }
+                    }
+                    
                     var endPoint = Instantiate(pointPrefab, new Vector3(startPointPosition.x + defaultTaskDuration,
                         startPointPosition.y, startPointPosition.z), Quaternion.identity);
                     taskItem.Init(milestoneTask, startPointPosition, endPoint.transform.position);
@@ -69,6 +94,15 @@ namespace GraphVisual
                     allTasks.Add(taskItem); 
                 }
             }
+        }
+
+        private void CreateLine(Vector3 startPosition, Vector3 endPosition)
+        {
+            var line = Instantiate(LinePrefab);
+            var lineRenderer = line.GetComponent<LineRenderer>();
+            
+            lineRenderer.SetPosition(0, startPosition);
+            lineRenderer.SetPosition(1, endPosition);
         }
     }
 }
--- Assets/Scripts/TaskItem.cs
+++ Assets/Scripts/TaskItem.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
+using TMPro;
 
 namespace GraphVisual
 {
     public class TaskItem : MonoBehaviour
     {
-        public string id;
+        public TMP_Text taskName;
+        
+        public string ID { get; private set; }
+        public bool IsMultiParent { get; private set; }
+        public bool IsFirstChildSet { get; set; }
 
-        public Vector3 startPoint;
-        public Vector3 endPoint;
+        private Vector3 StartPoint { get; set; }
+        public Vector3 EndPoint { get; set; }
 
         public void Init(MilestoneTask milestoneTask, Vector3 startPoint, Vector3 endPoint)
         {
-            id = milestoneTask.id;
-            this.startPoint = startPoint;
-            this.endPoint = endPoint;
+            ID = milestoneTask.id;
+            IsMultiParent = milestoneTask.isMultiParent;
+            IsFirstChildSet = false;
+
+            taskName.text = milestoneTask.id;
+            
+            StartPoint = startPoint;
+            EndPoint = endPoint;
             
             Debug.Log("startPoint = " + startPoint);
             Debug.Log("endPoint = " + endPoint);
